2394c7c29da680bcd4b232f4c3edaed762218664
Pavel Sushkevich <p.sushkevich@profigroup.by>
2018-12-11 14:26:50 +0300

    init commit

---
+++ apps/kv/lib/kv.ex
@@ -0,0 +1,23 @@
+defmodule KV do
+  @moduledoc """
+  Documentation for KV.
+  """
+  use Application
+
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KV.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
